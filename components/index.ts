d480681a3fa2d82496306cf492866bcfb9c8aa26
Ge1u <2723579248@qq.com>
2024-08-06 15:55:59 +0800

    search manufacturer

--- components/index.ts
+++ components/index.ts
@@ -2,10 +2,16 @@ import Hero from "@/components/Hero";
 import CustomButton from "@/components/CustomButton";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import SearchBar from "@/components/SearchBar";
+import CustomFilter from "@/components/CustomFilter";
+import SearchManufacturer from "@/components/SearchManufacturer";
 
 export {
     Hero,
     CustomButton,
     Navbar,
-    Footer
+    Footer,
+    CustomFilter,
+    SearchBar,
+    SearchManufacturer
 }
